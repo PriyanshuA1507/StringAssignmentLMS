d9c73d31b0bc7f0aaeff4901debdbc2036ff6157
PriyanshuA1507 <priyanshu_se21a15_07@dtu.ac.in>
2025-01-07 21:27:57 +0530

    Library Management System
    commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI:
+        'mongodb+srv://priyanshuse21a15:priyanshu@cluster1.h0zop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
+    JWT_SECRET: "ABCDEFGH",
+};
---
+++ routes/book_route.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const Book = require("../models/book");
+const Issue = require("../models/issue");
+
+router.post("/addBook", async (req, res) => {
+
+   const { title,author,publisher,year,copies } = req.body ;
+     console.log("req.body",req.body)
+    if(req.body._id){
+        const obj = await Issue.find({_id:req.body._id})
+        obj[0].isRecom = false 
+        await obj[0].save()
+    }
+    const book = await new Book({ title,author,publisher,year,copies})
+    await book.save()
+ 
+})
+router.get("/allBook", (req, res) => {
+    Book.find().sort({ createdAt: -1 }).then(data => {
+        res.status(200).json(
+           data
+        );
+    });
+});
+   
+
+
+
+module.exports = router;
